170ca29dbb9844a4583a299efc29c3f3b092d73b
Seyoung Yoon <stacey.exp@gmail.com>
2021-02-02 14:09:29 -0800

    navigation switches and router added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@
 import NavHeader from './components/navheader/';
 import ImpactPage from './pages/impact/';
 import Footer from './components/footer/index';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
@@ -10,6 +12,21 @@ function App() {
       <NavHeader></NavHeader>
       <ImpactPage></ImpactPage>
       <Footer></Footer>
+      <Router>
+          {/* <Navigation /> */}
+          <Switch>
+
+              {/* <Route exact path="/login" component={Splash}/> */}
+              <Route exact path="/people" component={Footer}/>
+              <Route exact path="/impact" component={Footer}/>
+              <Route exact path="/work" component={Footer}/>
+              <Route exact path="/partners" component={Footer}/>
+              <Route exact path="/news" component={Footer}/>
+              <Route exact path="/contact" component={Footer}/>
+            </Switch>
+      
+        
+          </Router>
     </div>
   );
 }
