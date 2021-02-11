60ec01524fe9672e255a77c475ae25265299a4e2
Giovanni Noisy <gnoisy77@gmail.com>
2021-02-11 13:23:37 -0800

    layout for work page

--- client/src/pages/work/index.js
+++ client/src/pages/work/index.js
@@ -1,125 +1,336 @@
 // WORK PAGE
 
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import Banner from '../../components/banner/';
-import './style.css';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Banner from "../../components/banner/";
+import "./style.css";
 
 export default function WorkPage() {
+  return (
+    <div>
+      <Banner></Banner>
 
-    return (
+      <Container className="proj-btns">
+        <div className="submenu">
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={6}>
+              <Button
+                variant="outlined"
+                className="submenu-button"
+                href="#p-area"
+              >
+                Project Areas
+              </Button>
+            </Grid>
 
-        <div>
-
-            <Banner></Banner>
-
-            <Container className="main">
-
-                <div className="submenu">
-
-                    <Grid container spacing={1}>
-
-                        <Grid item xs={6}>
-                            <Button variant="outlined" className="submenu-button">Project Areas</Button>
-                        </Grid>
-
-                        <Grid item xs={6}>
-                            <Button variant="outlined" className="submenu-button">Geographic Focus</Button>
-                        </Grid>
-
-                    </Grid>
-
-                </div>
-
-            </Container>
-            {/* BUILD ELEMENTS BELOW HERE. FEEL FREE TO EDIT THE ITEMS, JUST A TOOLKIT. */}
-
-
-    <Container className="proj-area">
-
-            <Typography
-                variant="h4"
-                className="proj-area-header"
-                style={{ textAlign: "center" }}
-            >
-                    Project Areas
-          </Typography>
-
-                <Container className="proj-card-container">
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Cybersecurity</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Quality Engineering</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Data Analytics</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Database Management</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Application Support</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Network Engineering</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Mainframe Computing</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>Web Development</Typography>
-                            </Box>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6} md={4}>
-                            <Box className="proj-card">
-                                <Typography variant="subtitle2" style={{ textAlign: "center" }}>IT Support</Typography>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Container>
-
-                <Container>
-                    <Typography variant="h4" style={{ textAlign: "center" }}>
-                        Geograhic Focus
-            </Typography>
-                </Container>
-            </Container>
-
-
-
-
-
-
+            <Grid item xs={12} sm={6}>
+              <Button
+                variant="outlined"
+                className="submenu-button"
+                href="#g-focus"
+              >
+                Geographic Focus
+              </Button>
+            </Grid>
+          </Grid>
         </div>
-    )
+      </Container>
+
+      <Container className="proj-card-container" id="p-area">
+        <Typography
+          variant="h3"
+          className="proj-area-header"
+          style={{ textAlign: "center", fontWeight: "900" }}
+        >
+          Project Areas
+        </Typography>
+
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Cybersecurity
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Endpoint protection; triage between endpoint, Active
+                  Directory, and remote management/monitoring tools;
+                  synchronization, auditing and hardening of Active Directory;
+                  critical information protection.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Quality Engineering
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Leading open source technologies, Agile & Scrum methodologies,
+                  automated product testing, mobile and web applications.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Data Analytics
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Abstraction, analysis & automation, object-oriented & Java
+                  programming. Project-based, using tools such as Eclipse IDE.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Database Management
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Extract, scrub and manipulate real-time and warehouse data,
+                  SQL & Java, Big Data and Hadoop. Project definition and test
+                  design for solutions, large data set preparation.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Application Support
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Troubleshoot & prioritize app coding problems (in Java or C+),
+                  engagement lifecycle stages, “Zero Maintenance Approach.”
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Network Engineering
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  CISCO CCNA certification; topologies and advanced config;
+                  monitoring and troubleshooting; servers; ITIL fundamentals;
+                  cloud and mobile computing.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Mainframe Computing
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Operations, Z/OS environment, terminology & skills.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  Web Development
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  HTML, CSS and JavaScript for full stack web development.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper>
+              <Box className="proj-card">
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                  className="proj-type-head"
+                >
+                  IT Support
+                </Typography>
+                <hr />
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  className="proj-type"
+                  style={{ textAlign: "center" }}
+                >
+                  Professional level OS (Windows, MacOS, Linux), hardware
+                  installation, maintenance troubleshooting.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container className="geo-focus-container" id="g-focus" mt={10}>
+        <Box className="geo-focus-img">
+          <Box pt={5}>
+            <Typography
+              variant="h3"
+              style={{ textAlign: "center", fontWeight: "900", color: "white" }}
+            >
+              Geographic Focus
+            </Typography>
+          </Box>
+        </Box>
+
+        <Container>
+          <Box p={10}>
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              style={{ textAlign: "center" }}
+            >
+              Our Associates are able to work on projects for clients throughout
+              the US. We work virtually, at our customers premises, or in a
+              hybrid framework. We are flexible to meet our customers' needs,
+              with each team employing the same innovative ecosystem model.
+              While we are a US company and our employees are drawn from
+              "domestic emerging markets" in the United States, we recognize the
+              global needs of some of our customers. Our founder and leadership
+              team have extensive experience in all the major international
+              markets and we will partner as needed to deliver integrated
+              offshore services.
+            </Typography>
+          </Box>
+        </Container>
+      </Container>
+
+      <Container className="offices-container">
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6}></Grid>
+        </Grid>
+      </Container>
+
+      {/* BUILD ELEMENTS BELOW HERE. FEEL FREE TO EDIT THE ITEMS, JUST A TOOLKIT. */}
+
+      {/* <Container className="proj-area">
+        <Container className="proj-card-container"></Container>
+
+        <Container>
+          <Typography variant="h4" style={{ textAlign: "center" }}>
+            Geograhic Focus
+          </Typography>
+        </Container>
+      </Container> */}
+    </div>
+  );
 }
