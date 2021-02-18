33a5ae72332f9e9343e708dec50736cecffa595e
Seyoung Yoon <stacey.exp@gmail.com>
2021-02-17 16:39:02 -0800

    partners page

--- client/src/pages/partners/index.js
+++ client/src/pages/partners/index.js
@@ -3,20 +3,36 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Box from "@material-ui/core/Box";
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Banner from '../../components/banner/';
+import { makeStyles } from '@material-ui/core/styles';
+import Footer from '../../components/footer'
 import './style.css';
 
-export default function PartnersPage() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            // margin: theme.spacing(1),
+            width: theme.spacing(40),
+            height: theme.spacing(50)
+        },
+    },
+}));
 
+export default function PartnersPage() {
+    const classes = useStyles();
     return (
 
         <div>
 
             <Banner></Banner>
-
+            <br></br>
+            <br></br>
             <Container className="main">
 
                 <div className="submenu">
@@ -45,42 +61,290 @@ export default function PartnersPage() {
 
                 {/* BUILD ELEMENTS BELOW HERE. FEEL FREE TO EDIT THE ITEMS, JUST A TOOLKIT. */}
 
-                <div id="founderdiv">
 
-                    <Paper className="padded-section">
+                <div id="talent">
 
-                        <Grid container spacing={1}>
 
-                            <Grid item xs={12}>
 
-                                <Typography variant="h3">Talent</Typography>
-                                <Typography variant="body1">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
-                                Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Egestas
-                                integer eget aliquet nibh praesent tristique. Imperdiet massa tincidunt nunc pulvinar sapien et.
-                                Vel turpis nunc eget lorem dolor sed viverra. Montes nascetur ridiculus mus mauris vitae.
-                                Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Ultricies mi quis hendrerit
-                                dolor magna. Dolor morbi non arcu risus quis varius. Fames ac turpis egestas integer.
-                                Amet nisl purus in mollis nunc sed. Neque viverra justo nec ultrices dui sapien eget
-                                mi proin.
-
+                    <Typography variant="h3" style={{ textAlign: "center", fontWeight: "900", marginTop: "8%" }}>Talent Partners</Typography>
+                    <Typography variant="body1" style={{ textAlign: "center", margin: "3%", marginRight: "10%", marginLeft: "10%" }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
                                 </Typography>
 
-                            </Grid>
-
-                        </Grid>
-
-                    </Paper>
 
                 </div>
 
 
+                <Grid container spacing={5}>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Paper >
+                            <Box className="proj-card">
+                                <Typography
+                                    variant="h6"
+                                    gutterBottom
+                                    style={{ textAlign: "center" }}
+                                    className="proj-type-head"
+                                >
+                                    Cybersecurity
+                                </Typography>
+                                <hr />
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className="proj-type"
+                                    style={{ textAlign: "center" }}
+                                >
+                                    Endpoint protection; triage between endpoint, Active
+                                    Directory, and remote management/monitoring tools;
+                                    synchronization, auditing and hardening of Active Directory;
+                                    critical information protection.
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Paper >
+                            <Box className="proj-card">
+                                <Typography
+                                    variant="h6"
+                                    gutterBottom
+                                    style={{ textAlign: "center" }}
+                                    className="proj-type-head"
+                                >
+                                    Cybersecurity
+                                </Typography>
+                                <hr />
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className="proj-type"
+                                    style={{ textAlign: "center" }}
+                                >
+                                    Endpoint protection; triage between endpoint, Active
+                                    Directory, and remote management/monitoring tools;
+                                    synchronization, auditing and hardening of Active Directory;
+                                    critical information protection.
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Paper >
+                            <Box className="proj-card">
+                                <img src=''></img>
+                                <Typography
+                                    variant="h6"
+                                    gutterBottom
+                                    style={{ textAlign: "center" }}
+                                    className="proj-type-head"
+                                >
+                                    Cybersecurity
+                                </Typography>
+                                <hr />
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className="proj-type"
+                                    style={{ textAlign: "center" }}
+                                >
+                                    Endpoint protection; triage between endpoint, Active
+                                    Directory, and remote management/monitoring tools;
+                                    synchronization, auditing and hardening of Active Directory;
+                                    critical information protection.
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    </Grid>
+                </Grid>
+
+                <div id="training">
+
+                    <Typography variant="h3" style={{ textAlign: "center", fontWeight: "900", marginTop: "8%" }}>Training Partners</Typography>
+                    <Typography variant="body1" style={{ textAlign: "center", margin: "3%", marginRight: "10%", marginLeft: "10%" }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
+                    </Typography>
+
+
+
+                </div>
+
+                <Grid container spacing={5}>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                                </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                                </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                                </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                                </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <img src=''></img>
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                                </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                                </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+
+
+                <div id="academic">
+
+                    <Typography variant="h3" style={{ textAlign: "center", fontWeight: "900", marginTop: "8%", marginBottom: "0%" }}>Academic Partners</Typography>
+                    <Typography variant="body1" style={{ textAlign: "center", margin: "3%", marginRight: "10%", marginLeft: "10%" }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        euismod in pellentesque. Pellentesque id nibh tortor id aliquet lectus proin.
+                    </Typography>
+
+
+
+                </div>
+
+                <Grid container spacing={5}>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                            </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                            </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid className={classes.root} item xs={12} sm={6} md={4}>
+                        <Box className="proj-card">
+                            <img src=''></img>
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                style={{ textAlign: "center" }}
+                                className="proj-type-head"
+                            >
+                                Cybersecurity
+                            </Typography>
+                            <hr />
+                            <Typography
+                                variant="body2"
+                                gutterBottom
+                                className="proj-type"
+                                style={{ textAlign: "center" }}
+                            >
+                                Endpoint protection; triage between endpoint, Active
+                                Directory, and remote management/monitoring tools;
+                                synchronization, auditing and hardening of Active Directory;
+                                critical information protection.
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+
 
             </Container>
 
-
-
         </div>
+
     )
 }
