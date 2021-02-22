f7018cacd0aa673e7193af1fa58b132221c42b9a
Jen Jayme <jeniferjayme@gmail.com>
2021-02-21 19:19:50 -0800

    Optimized images, added staff cards and Divider component

---
+++ client/src/components/divider/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import dividerimg from './border-rt-bottom-orange-min.png';
+import './style.css';
+
+export default function Divider () {
+
+    return (
+  
+      <div>
+  
+        <img src={dividerimg} alt="divider image" className="divider" width="100%"></img> 
+  
+      </div>
+  
+    )
+  }
