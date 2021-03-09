b7db74d349793c25bc69fe916ce79d1fb9943395
Seyoung Yoon <stacey.exp@gmail.com>
2021-03-09 12:33:45 -0800

    nav bar scroll visibility

--- client/src/App.js
+++ client/src/App.js
@@ -45,56 +45,69 @@ function App() {
       console.log("Menu icon clicked")
     })
   }
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("menu").style.backgroundColor = "black";
+
+    }
+    else{
+        document.getElementById("menu").style.backgroundColor = "transparent";
+
+    }
+  }
 
 
-  return (
-    <div className="App">
+    return (
 
-      <Router>
+      <div className="App">
 
-        <nav id='navBar' className="header-outer">
-          {/* <Banner></Banner> */}
-          <div id="smallScreenNavBar" className="hideOnMedUp header-inner responsive-wrapper">
-            <MenuIcon id="menuIcon" className="hideOnMedUp" onClick={handleMenuClick}></MenuIcon></div>
-          <ul className="menu">
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/people" activeClassName="selectedLink">People</NavLink>
-            </li>
-            <li>
-              <NavLink to="/impact" activeClassName="selectedLink">Impact</NavLink>
-            </li>
-            <li>
-              <NavLink to="/work" activeClassName="selectedLink">Work</NavLink>
-            </li>
-            <li>
-              <NavLink to="/partners" activeClassName="selectedLink">Partners</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" activeClassName="selectedLink">Contact</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Router>
 
-        <Switch>
+          <nav id='navBar' className="header-outer">
+            {/* <Banner></Banner> */}
+            <div id="smallScreenNavBar" className="hideOnMedUp header-inner responsive-wrapper">
+              <MenuIcon id="menuIcon" className="hideOnMedUp" onClick={handleMenuClick}></MenuIcon></div>
+            <ul className="menu" id='menu'>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/people" activeClassName="selectedLink">People</NavLink>
+              </li>
+              <li>
+                <NavLink to="/impact" activeClassName="selectedLink">Impact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/work" activeClassName="selectedLink">Work</NavLink>
+              </li>
+              <li>
+                <NavLink to="/partners" activeClassName="selectedLink">Partners</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" activeClassName="selectedLink">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
 
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/people" component={PeoplePage} />
-          <Route exact path="/impact" component={ImpactPage} />
-          <Route exact path="/work" component={WorkPage} />
-          <Route exact path="/partners" component={PartnersPage} />
-          <Route exact path="/news" component={NewsPage} />
-          <Route exact path="/contact" component={ContactPage} />
+          <Switch>
 
-        </Switch>
-      </Router>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/people" component={PeoplePage} />
+            <Route exact path="/impact" component={ImpactPage} />
+            <Route exact path="/work" component={WorkPage} />
+            <Route exact path="/partners" component={PartnersPage} />
+            <Route exact path="/news" component={NewsPage} />
+            <Route exact path="/contact" component={ContactPage} />
 
-      <Footer></Footer>
+          </Switch>
+        </Router>
 
-    </div>
-  );
-}
+        <Footer></Footer>
 
-export default App;
+      </div>
+    );
+  }
+
+  export default App;
